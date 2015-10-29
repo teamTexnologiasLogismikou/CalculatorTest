cdcd840af27db6bb2eda649e2cf4c2d34173c792
stellaTer <stella_terzi119@hotmail.com>
2015-10-29 14:59:59 +0200

    plus Test

---
+++ test/tests/plusTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class plusTest {
+    
+    public plusTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class plus.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        String s1 = "2";
+        String s2 = "2";
+        plus instance = new plus();
+        double expResult = 4;
+        double result = instance.add(s1, s2);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+     @Test
+    public void testAdd2() {
+        System.out.println("add");
+        String s1 = "2";
+        String s2 = "6";
+        plus instance = new plus();
+        double expResult = 8;
+        double result = instance.add(s1, s2);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+     @Test
+    public void testAdd3() {
+        System.out.println("add");
+        String s1 = "2";
+        String s2 = "-3";
+        plus instance = new plus();
+        double expResult = -1;
+        double result = instance.add(s1, s2);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+     @Test
+    public void testAdd4() {
+        System.out.println("add");
+        String s1 = "2.3";
+        String s2 = "5.4";
+        plus instance = new plus();
+        double expResult = 7.7;
+        double result = instance.add(s1, s2);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+}
